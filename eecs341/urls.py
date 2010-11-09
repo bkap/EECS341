06d8431ff9dcc325d3530c84c917971fdeb60f39
Ben Kaplan <benjamin.kaplan@case.edu>
2010-11-08 21:14:53 -0500

    Added basic readme and modified urls.py to pass stuff on to admin and scheduler.

--- eecs341/urls.py
+++ eecs341/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^eecs341/', include('eecs341.foo.urls')),
+     (r'^scheduler/', include('eecs341.scheduler.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+     (r'^admin/', include(admin.site.urls)),
 )
