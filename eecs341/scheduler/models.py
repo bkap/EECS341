71090e4ccfd71f0b01bd9521e190da1c374493e3
Ben Kaplan <benjamin.kaplan@case.edu>
2010-11-08 21:27:20 -0500

    Fixed user to use the built-in authentication.

--- eecs341/scheduler/models.py
+++ eecs341/scheduler/models.py
@@ -1,12 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class User(models.Model) :
-	name = models.CharField(max_length=60)
-	ROLE_CHOICES = ( ('S','student'),
-					 ('P','proessor'),
-					 ('A','administrator') )
-	role = models.CharField(max_length=1,choices=ROLE_CHOICES)
 
 class Semester(models.Model) :
 	name = models.CharField(max_length=20,primary_key=True)
