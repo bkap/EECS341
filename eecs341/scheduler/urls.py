3d081c229fa2333d6893576d141169b5c5e0c905
Ben Kaplan <benjamin.kaplan@case.edu>
2010-11-22 00:48:52 -0500

    started work on search views. Probably not working correctly right now.

--- eecs341/scheduler/urls.py
+++ eecs341/scheduler/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
-from views import login_page, hello
+from views import login_page, hello,search_form
 urlpatterns = patterns('',
 	(r'login.html',login_page),
-	(r'welcome.html',hello))
+	(r'welcome.html',hello),
+	(r'search.html',search_form),)
--- eecs341/scheduler/views.py
+++ eecs341/scheduler/views.py
@@ -27,3 +27,22 @@ def hello(request) :
 			return render_to_response('welcome.html',{'user':request.user})
 		else :
 			return HttpResponse("Please Login")	
+
+def search(request) :
+	return render_to_response('search_form.html',{})
+def searchresults(request) :
+	dept = request.GET['dept'] or None
+	sem_name = request.GET['semester'] or None
+	course_num = request.GET['coursenum'] or None
+	professor = request.GET['professor'] or None
+	order = request.GET['ratio']
+	semester = Semester.objects.get(name=sem_name)
+	if semester is None :
+		return HttpResponse("Error: unable to find semester")
+	coursenum__gt=int(course_num) if order == 'gt' else None
+	coursenum__lt = int(course_num) if order=='lt' else None
+	coursenum = int(course_num) if order=="eq" else None
+	
+	#TODO: figrue out how to do this query efficiently
+	return render_to_response("search.html",{user:request.user,
+	courses:Class.objects.all()})
