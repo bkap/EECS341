5e2ef7e0d5c7001a1399550b33eb0a25b1779b52
Donald Huckle <drh32@case.edu>
2010-11-22 22:16:52 -0500

    Configured the Database on this computer.  I am using mysql3 for the database.  X

--- eecs341/settings.py
+++ eecs341/settings.py
@@ -10,12 +10,12 @@ MANAGERS = ADMINS
 FIXTURE_DIRS = ("scheduler/fixtures/",)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'eecs341',                      # Or path to database file if using sqlite3.
-        'USER': 'eecs341',                      # Not used with sqlite3.
-        'PASSWORD': 'eecs341',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', 		# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mydb.db',  # Or path to database file if using sqlite3.
+        #'USER': 'eecs341',                      		# Not used with sqlite3.
+        #'PASSWORD': 'eecs341',                  		# Not used with sqlite3.
+        #'HOST': '',                      			# Set to empty string for localhost. Not used with sqlite3.
+        #'PORT': '',                      			# Set to empty string for default. Not used with sqlite3.
     }
 }
 
